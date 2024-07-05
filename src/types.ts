b99b28d986487e999edeb7d1030cf1796ff798c1
Vincent Scavetta <vstro24@gmail.com>
2024-07-05 02:14:49 -0400

    WIP - Refactoring hard coded elements to be based on arrays + some style changes

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type ProjectMetaData = {
+	title: string
+	description?: string
+    href?: string | undefined
+	imagePath?: string | undefined
+	altText?: string | undefined
+    customStyle?: string | undefined
+}
